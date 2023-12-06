c73858cb2b379fd7d1dfb8964496187224048d00
Akari Katsuma <katainu0103@gmail.com>
2023-12-06 22:19:44 +0900

    Add URL for get quiz data

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 app = FastAPI()
+
+# test data
+data = {
+    str(i): f"item{i}" for i in range(1, 81)
+}
 
 @app.get("/")
 async def root():
  return {"Hello": "World",}
+
+@app.get("/quiz/{quiz_id}")
+async def get_quiz(quiz_id: str):
+    if quiz_id not in data:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"quiz_id": quiz_id, "quiz": data[quiz_id]}
