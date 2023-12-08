e341ee133fd0b921350d28db26f6ce8468ec66c4
Akari Katsuma <katainu0103@gmail.com>
2023-12-08 22:02:20 +0900

    Solve warning

--- CRUD.py
+++ CRUD.py
@@ -12,7 +12,7 @@ def get_records(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Record).offset(skip).limit(limit).all()
 
 def create_record(db: Session, record_data: schemas.RecordCreate):
-    new_record = Record(**record_data.dict())
+    new_record = Record(**record_data.model_dump())
     db.add(new_record)
     db.commit()
     db.refresh(new_record)
@@ -25,7 +25,7 @@ def get_predictions(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Prediction).offset(skip).limit(limit).all()
 
 def create_prediction(db: Session, prediction_data: schemas.PredictionCreate):
-    new_prediction = Prediction(**prediction_data.dict())
+    new_prediction = Prediction(**prediction_data.model_dump())
     db.add(new_prediction)
     db.commit()
     db.refresh(new_prediction)
