5e0c9bbf114cc2f36693ec8944e44563c87949d3
Akari Katsuma <katainu0103@gmail.com>
2023-12-08 16:49:19 +0900

    Add test

--- test_.py
+++ test_.py
@@ -8,6 +8,9 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
+#==================================
+# 問題文取得
+#==================================
 def test_get_collectID_1():
     quiz_id = 1
     response = client.get(f"/quiz/{quiz_id}")
@@ -29,44 +32,57 @@ def test_get_wrongID():
     response = client.get(f"/quiz/{quiz_id}")
     assert response.status_code == 404
 
+#==================================
+# 回答送信
+#==================================
 def test_post_collectID_1():
     quiz_id = 1
     response = client.post(f"/quiz/{quiz_id}?answer=Real")
+    response_json = response.json()
     assert response.status_code == 200
-    assert response.json() == {
-        "result_hash_id": "random-hash-string"
-    }
+    assert isinstance(response_json["result_hash_id"], str)
 
 def test_post_collectID_2():
     quiz_id = 100
     response = client.post(f"/quiz/{quiz_id}?answer=Fake")
+    response_json = response.json()
     assert response.status_code == 200
-    assert response.json() == {
-        "result_hash_id": "random-hash-string"
-    }
-
+    assert isinstance(response_json["result_hash_id"], str)
 
 def test_post_collectFormat_1():
     response = client.post(f"/quiz/1?answer=Fake")
+    response_json = response.json()
     assert response.status_code == 200
-    assert response.json() == {
-        "result_hash_id": "random-hash-string"
-    }
+    assert isinstance(response_json["result_hash_id"], str)
     
-
 def test_post_collectFormat_2():
     response = client.post(f"/quiz/1?answer=Real")
+    response_json = response.json()
     assert response.status_code == 200
-    assert response.json() == {
-        "result_hash_id": "random-hash-string"
-    }
+    assert isinstance(response_json["result_hash_id"], str)
 
 def test_post_wrongID():
     quiz_id = -1
     response = client.post(f"/quiz/{quiz_id}?answer=Fake")
     assert response.status_code == 404
-    
 
 def test_post_wrongFormat():
     response = client.post(f"/quiz/1?answer=aaa")
     assert response.status_code == 400
+
+
+#==================================
+# 戦歴取得
+#==================================
+def test_get_result_collectID():
+    hash_id = "random-hash-string"
+    response = client.get(f"/result/{hash_id}")
+    response_json = response.json()
+    assert response.status_code == 200
+    assert isinstance(response_json["isCollect"], int)
+
+def test_get_result_wrongID():
+    hash_id = "not-exist-ID"
+    response = client.get(f"/result/{hash_id}")
+    response_json = response.json()
+    assert response.status_code == 404
