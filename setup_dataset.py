c0c9f8904db0d2335401c88ee39c74723c999849
Akari Katsuma <katainu0103@gmail.com>
2023-12-13 04:23:55 +0900

    Add preprocessing function

--- setup_dataset.py
+++ setup_dataset.py
@@ -1,5 +1,8 @@
+import copy
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
+from transformers import AutoModel
 
 #================================
 # エンコーダー
@@ -42,6 +45,12 @@ def encoder_education(text):
     }
     return int_convert(education_levels.get(text, np.nan))
 
+def split_columns_salary(df):
+    df['salary_lower'] = df['salary_range'].str.split('-').str[0].apply(int_convert)
+    df['salary_upper'] = df['salary_range'].str.split('-').str[1].apply(int_convert)
+    df = df.drop('salary_range', axis=1)
+    return df
+
 #================================
 # データセット管理クラス
 #================================
@@ -73,12 +82,46 @@ class Dataset:
         traindf = traindf.sample(frac=1).reset_index(drop=True)
         testdf = testdf.sample(frac=1).reset_index(drop=True)
 
-        train_X = testdf.drop('fraudulent', axis=1)
         self.train_y = testdf['fraudulent']
-        test_X = testdf.drop('fraudulent', axis=1)
         self.test_y = testdf['fraudulent']
+        train_X = testdf.drop('fraudulent', axis=1)
+        test_X = testdf.drop('fraudulent', axis=1)
+        
+        # 学習済みembeddingsモデルを外部からダウンロード
+        self.emb_model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-small-en', trust_remote_code=True) # trust_remote_code is needed to use the encode method
 
-        # ここでデータ加工
+        # データ前処理
+        self.train_X = self.preprocessing(train_X)
+        self.test_X = self.preprocessing(test_X)
 
-        self.train_X = train_X
-        self.test_X = test_X
+
+    def preprocessing(self, df):
+        # 要求経歴レベルを、文字列から数値に変換
+        df['required_experience'] = df['required_experience'].apply(encoder_experience)
+        df['required_education'] = df['required_education'].apply(encoder_education)
+
+        # 給料範囲を上限と下限の2つのカラムに分ける
+        df = split_columns_salary(df)
+        df = split_columns_salary(df)
+
+        # 不要な列の削除
+        selected_columns = ['company_profile', 'description', 'benefits', 'required_experience', 'required_education', 'salary_lower', 'salary_upper']
+        text_columns = ['company_profile', 'description', 'benefits']
+        processed_df = copy.deepcopy(df[selected_columns])
+
+        # 文字列をembedding
+        for tc in text_columns:
+            print(f"Now embedding {tc}...")
+            embeddings = self.emb_model.encode(processed_df[tc].fillna(''))
+            print(f"{tc} is encoded.")
+
+            # 行列の各列をDataFrameの新たなカラムとして保存
+            columns = []
+            for i in range(embeddings.shape[1]):
+                    columns.append(f'{tc}_{i}')
+            emb_df = pd.DataFrame(embeddings, columns=columns)
+            processed_df = pd.concat([processed_df, emb_df], axis=1)
+
+            processed_df = processed_df.drop(tc, axis=1)
+        
+        return processed_df
